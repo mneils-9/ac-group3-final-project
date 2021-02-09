604c341c5860813bd2ae3384f97be53a59b81146
klobby19 <klobby19@gmail.com>
2021-02-08 16:42:48 -0800

    added script and visuals

--- analysis.R
+++ analysis.R
@@ -1,11 +1,29 @@
 library(tidyverse)
 
-data <- spreadspoke_scores
+scores_df <- read.csv("spreadspoke_scores.csv")
 
-data <- data %>% mutate(home_win = (score_home > score_away))
+scores_df <- scores_df %>% mutate(home_win = (score_home > score_away))
 
-home_wins_season <- data %>% 
+prop_home_win <- scores_df %>% 
   group_by(schedule_season) %>% 
   filter(home_win == TRUE, .preserve = TRUE) %>% 
   summarise(prop = n()) %>%
-  mutate(prop = prop / sum(prop))
+  mutate(prop_home_wins = prop / sum(prop))
+
+spread_scores_df <- scores_df %>% filter(!is.na(spread_favorite))
+
+avg_spreads <- spread_scores_df %>% 
+  group_by(schedule_season) %>% 
+  summarize(avg_spread = mean(spread_favorite))
+
+year_score_df <- left_join(prop_home_win, avg_spreads)
+
+sb_spreads <- spread_scores_df %>% 
+  filter(schedule_week == "Superbowl") 
+
+ggplot(data = sb_spreads) +
+  geom_line(mapping = aes(x = schedule_season, y = spread_favorite))
+
+ggplot() +
+  geom_line(data = year_score_df, aes(x = schedule_season, y = prop), color = "darkcyan") +
+  ggsave("homewins.png")
