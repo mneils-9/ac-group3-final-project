c34d8e32bb332bbb0ae18ee7f83346c0ae0bcda3
Maddie <>
2021-02-16 18:36:15 -0800

    starting chart 1

--- chart_1.R
+++ chart_1.R
@@ -1,6 +1,7 @@
 # Maddie: Chart: are home teams still favorited in 2020 season?
 
 library(tidyverse)
+library(ggplot2)
 
 # Read in data.
 scores_df <- read.csv("data:/spreadspoke_scores.csv")
@@ -9,4 +10,6 @@ teams <- read.csv("data:/teamanno.csv")
 source(light_analysis.R)
 
 projection_accuracy <- spread_scores_df$favorite_win
+
+
 
