3f213b71fc074fc7052bb6c3a6e374333ab6de64
Bryan Bunyatipanon <Bryanb1@uw.edu>
2021-02-18 12:01:24 -0800

    commit

--- scripts:/table.R
+++ scripts:/table.R
@@ -1,3 +1,3 @@
 library(tidyverse)
 source("changedf.R")
-scores_df %>% group_by(schedule_week) %>% summarise(average_homescore=mean(score_home))
+summarytable <- scores_df %>% filter(team_favorite == team_home) %>% group_by(team_favorite) %>% summarise(average_homescore=mean(score_home))
