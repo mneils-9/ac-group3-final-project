2f5907d56cba059cec84a69739e51ce3ed7f08be
Maddie <>
2021-03-07 19:47:21 -0800

    added stuff from previous assignment to interactive visual page 1

--- shiny:/server.R
+++ shiny:/server.R
@@ -228,4 +228,29 @@ server <- function(input, output) {
   output$summary <- renderText({
     "blah blah blah"
   })
+  
+  output$bets_accuracy_plot <- renderPlotly({
+    # Dataframe for % accuracy of projections by year
+    projection_accuracy <- scores_df %>%
+      select(schedule_season, team_favorite, team_win, favorite_win,
+             percent_accuracy)
+    
+    yearly_projection_accuracy <- projection_accuracy %>%
+      group_by(schedule_season) %>%
+      summarise(mean(percent_accuracy))
+    
+    # Make plot!
+    plot1 <- ggplot(data = yearly_projection_accuracy) +
+      geom_point(mapping = aes(x = schedule_season, y = `mean(percent_accuracy)`),
+                 color = "#013369") +
+      geom_smooth(mapping = aes(x = schedule_season, y = `mean(percent_accuracy)`),
+                  color = "#D50A0A", method = "loess", formula = y ~x) +
+      theme_minimal() +
+      labs(
+        x = "Season (Year)",
+        y = "Percent Accuracy"
+      )
+    ggplotly(plot1)
+    
+  })
 }
